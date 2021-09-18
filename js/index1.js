c071ee94194b53f819fc93d26172e5e919b37937
itsdeveloperguy <87490298+itsdeveloperguy@users.noreply.github.com>
2021-09-18 15:37:36 +0530

    Add files via upload

---
+++ js/index1.js
@@ -0,0 +1,47 @@
+console.clear()
+function dev(){
+    var developercheak = document.getElementById("developercheak").value;
+    var devname = document.getElementById("devname");
+    if(developercheak == "Mohd.Anas"){
+        alert("You are now a developer")
+        devname.innerHTML = "You are now a developer";
+        document.getElementById("book").style.display = "block";
+    document.getElementsByClassName("developer")[0].style.display = "none";
+    }
+    else if(developercheak == "Monica Chopra"){
+        alert("You are a teacher");
+    devname.innerHTML =
+      "WELCOME ! <br> To the Teachers Mode. You have a valid Teacher certificate";
+    document.getElementsByClassName("developer")[0].style.display = "none";
+    document.getElementById("book").style.display = "block";
+    }
+    else if (developercheak == "Ravish"){
+        alert("!! You are a Khamba !! ");
+    devname.innerHTML = " <h1>WELCOME ! </h1> <br> To the Students Mode.";
+    document.getElementsByClassName("developer")[0].style.display = "none";
+    }
+    else if (developercheak == "Books"){
+        document.getElementById("book").style.display = "block";
+        document.getElementsByClassName("developer")[0].style.display = "none";
+    devname.innerHTML = "Books links are Created <h2>Sucessfully !! </h2>";
+    }
+    else if (developercheak == "books"){
+      document.getElementById("book").style.display = "block";
+      document.getElementsByClassName("developer")[0].style.display = "none";
+  devname.innerHTML = "Books links are Created <h2>Sucessfully !! </h2>";
+  }
+    else if (developercheak == ""){
+        console.log("Cheaking...")
+        alert("Please Enter your name");
+        document.getElementsByClassName("developer")[0].innerText = "Try again";
+    }
+}
+function search() {
+        var size = document.getElementById("developercheak");
+    if (document.getElementById("developercheak").style.width == "20vw") {
+      document.getElementById("developercheak").style.width = "30vw";
+    } else {
+      document.getElementById("developercheak").style.width = "20vw";
+    }
+  }
+  
